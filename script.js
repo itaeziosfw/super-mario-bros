ad6f656a562b6edacae65c2de5708da2216d3e55
itaeziosfw <itaezioj@gmail.com>
2024-01-13 17:42:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector(".fale-conosco")
+console.log(formulario)
+
+function cliqueiNobotao(){
+    formulario.style.left = "700px"
+}
+
+
+function sumirFormulario(){
+    formulario.style.left = "320px"
+}
